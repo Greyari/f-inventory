962eeff0f01953f71cac71631ac920bf42fed24a
Greyari <tupang1017@gmail.com>
2026-07-31 10:30:09 +0700

    changing the naming for i18n locales

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,8 +1,8 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-import id from "./locales/id/common.json";
-import en from "./locales/en/common.json";
+import id from "./locales/id.json";
+import en from "./locales/en.json";
 
 i18n
   .use(LanguageDetector)
